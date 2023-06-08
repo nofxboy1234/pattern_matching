acc24902b72807f508be0011f418a90d928ab2a1
Dylan Palmboom <nofxboy1234@gmail.com>
2023-06-08 12:02:35 +0200

    Update practice

--- pattern_matching.rb
+++ pattern_matching.rb
@@ -411,11 +411,95 @@
 #   puts 'not matched'
 # end
 
-case ['a', 1, 'b', 'c', 2]
-in [*, String, String, *]
-  puts 'matched'
+# case ['a', 1, 'b', 'c', 2]
+# in [*, String, String, *]
+#   puts 'matched'
+# else
+#   puts 'not matched'
+# end
+
+# case ['a', 1, 'b', 'c', 2]
+# in [*pre, String, String, *post]
+#   puts 'matched'
+#   p pre
+#   p post
+# else
+#   puts 'not matched'
+# end
+
+# case [1, 2]
+# in Integer => a, Integer
+#   puts "matched #{a}"
+# else
+#   puts 'not matched'
+# end
+
+# case [1, 2]
+# in [Integer => a, Integer]
+#   puts "matched #{a}"
+# else
+#   puts 'not matched'
+# end
+
+# case { a: 1, b: 2, c: 3 }
+# in a: Integer => m
+#   puts "matched: #{m}"
+# else
+#   puts 'not matched'
+# end
+
+# case { a: 1, b: 2, c: 3 }
+# in {a: Integer => m}
+#   puts "matched: #{m}"
+# else
+#   puts 'not matched'
+# end
+
+# case [1, 2]
+# in a, Integer
+#   puts "matched #{a}"
+# else
+#   puts 'not matched'
+# end
+
+# case { a: 1, b: 2, c: 3 }
+# in a: m
+#   puts "matched: #{m}"
+# else
+#   puts 'not matched'
+# end
+
+# case { a: 1, b: 2, c: 3 }
+# in a:
+#   puts "matched: #{a}"
+# else
+#   puts 'not matched'
+# end
+
+# case { a: 1, b: 2, c: 3 }
+# in a => m
+#   puts "matched: #{m}"
+# else
+#   puts 'not matched'
+# end
+
+case { name: 'John', friends: [{ name: 'Jane' }, { name: 'Rajesh' }] }
+in name:, friends: [{name: first_friend}, *]
+  puts "matched #{first_friend}"
 else
   puts 'not matched'
 end
 
+case [1, 2, 3]
+in a, *rest
+  puts "matched: #{a}, #{rest}"
+else
+  puts 'not matched'
+end
 
+case {a: 1, b: 2, c: 3}
+in a:, **rest
+  puts "matched: #{a}, #{rest}"
+else
+  puts 'not matched'
+end
