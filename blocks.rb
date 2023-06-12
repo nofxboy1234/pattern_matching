6773c14b04bbf7ed45646ac1b29cc1dbbd3333c7
Dylan Palmboom <nofxboy1234@gmail.com>
2023-06-12 14:18:22 +0200

    Update practice

--- blocks.rb
+++ blocks.rb
@@ -598,43 +598,23 @@
 # p a
 # p %i[test many keys].map(&{test: 1})  #=> [1, nil, nil]
 
-def m1(&b) = b.call
+# def m1(&b) = b.call
 
-def m2
-  m1 { return }
-end
-p m2 # ok
+# def m2
+#   m1 { return }
+# end
+# p m2 # ok
 
-def m1(&b) = b.call
+# def m1(&b) = b.call
 
-def m2
-  m1 { break }
-end
-p m2 # ok
+# def m2
+#   m1 { break }
+# end
+# p m2 # ok
 
 def m1(&) = b
 
 def m2
   m1 { return }.call
 end
-p m2 # ok
-
-# def m1(&) = b
-
-# def m2
-#   m1 { break }.call
-# end
-# m2 # LocalJumpError
-
-# def m1(&) = b
-
-# def m2
-#   m1 { return }
-# end
-# m2.call # LocalJumpError
-# def m1(&) = b
-
-# def m2
-#   m1 { break }
-# end
-# m2.call # LocalJumpError
+m2 # ok
